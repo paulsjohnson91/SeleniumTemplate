34b567763e70b9f251f4f552c4605e40396db1c5
Paul Johnson <paul.sjohnson91@gmail.com>
2017-05-05 13:07:29 +0100

    Fixed support for Appium Chrome browser testing.

--- src/main/java/com/fujitsu/webframework/common/drivers/browsers/AndroidBrowser.java
+++ src/main/java/com/fujitsu/webframework/common/drivers/browsers/AndroidBrowser.java
@@ -5,6 +5,10 @@ import com.fujitsu.webframework.common.configuration.Configuration;
 import com.fujitsu.webframework.common.drivers.BrowserAbstract;
 import com.fujitsu.webframework.common.logging.PageObjectLogging;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -20,16 +24,19 @@ public class AndroidBrowser extends BrowserAbstract {
 
   @Override
   public void setOptions() {
-    DesiredCapabilities destCaps = new DesiredCapabilities();
-    destCaps.setCapability("deviceName", Configuration.getDeviceName());
-    URL url = null;
+    DesiredCapabilities capabilities= DesiredCapabilities.android();
+    capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.CHROME);
+    capabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.ANDROID);
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,Configuration.getDeviceName());
+    capabilities.setCapability(MobileCapabilityType.VERSION,"5.0.1");
+    URL url= null;
     try {
       url = new URL("http://" + Configuration.getAppiumIp() + "/wd/hub");
     } catch (MalformedURLException e) {
       PageObjectLogging.log("getAndroidInstance", e, false);
     }
-    mobileDriver = new AndroidDriver(url, destCaps);
-
+    mobileDriver = new AndroidDriver(url, capabilities);
   }
 
   @Override
